52ac096fcb5d24244e574f7c42158db49f2d7bec
Andrej Karpathy <andrej.karpathy@gmail.com>
2022-03-10 23:46:47 -0800

    sequester script code and save model to disk

--- repro.py
+++ repro.py
@@ -91,62 +91,67 @@ class Net(nn.Module):
 
 # -----------------------------------------------------------------------------
 
-parser = argparse.ArgumentParser(description="Train a 1989 LeCun ConvNet on digits")
-parser.add_argument('--learning-rate', '-l', type=float, default=0.03, help="SGD learning rate")
-parser.add_argument('--output-dir'   , '-o', type=str,   default='out/base', help="output directory for training logs")
-args = parser.parse_args()
-print(vars(args))
+if __name__ == '__main__':
 
-# inits
-torch.manual_seed(1337)
-np.random.seed(1337)
-os.makedirs(args.output_dir, exist_ok=True)
-with open(os.path.join(args.output_dir, 'args.json'), 'w') as f:
-    json.dump(vars(args), f, indent=2)
-writer = SummaryWriter(args.output_dir)
+    parser = argparse.ArgumentParser(description="Train a 1989 LeCun ConvNet on digits")
+    parser.add_argument('--learning-rate', '-l', type=float, default=0.03, help="SGD learning rate")
+    parser.add_argument('--output-dir'   , '-o', type=str,   default='out/base', help="output directory for training logs")
+    args = parser.parse_args()
+    print(vars(args))
 
-# init a model
-model = Net()
-print("number of params: ", sum(p.numel() for p in model.parameters())) # in paper total is 9,760
+    # inits
+    torch.manual_seed(1337)
+    np.random.seed(1337)
+    os.makedirs(args.output_dir, exist_ok=True)
+    with open(os.path.join(args.output_dir, 'args.json'), 'w') as f:
+        json.dump(vars(args), f, indent=2)
+    writer = SummaryWriter(args.output_dir)
 
-# init data
-Xtr, Ytr = torch.load('train1989.pt')
-Xte, Yte = torch.load('test1989.pt')
+    # init a model
+    model = Net()
+    print("number of params: ", sum(p.numel() for p in model.parameters())) # in paper total is 9,760
 
-# init optimizer
-optimizer = optim.SGD(model.parameters(), lr=args.learning_rate)
+    # init data
+    Xtr, Ytr = torch.load('train1989.pt')
+    Xte, Yte = torch.load('test1989.pt')
 
-def eval_split(split):
-    # eval the full train/test set, batched implementation for efficiency
-    model.eval()
-    X, Y = (Xtr, Ytr) if split == 'train' else (Xte, Yte)
-    Yhat = model(X)
-    loss = torch.mean((Y - Yhat)**2)
-    err = torch.mean((Y.argmax(dim=1) != Yhat.argmax(dim=1)).float())
-    print(f"eval: split {split:5s}. loss {loss.item():e}. error {err.item()*100:.2f}%. misses: {int(err.item()*Y.size(0))}")
-    writer.add_scalar(f'error/{split}', err.item()*100, pass_num)
-    writer.add_scalar(f'loss/{split}', loss.item(), pass_num)
+    # init optimizer
+    optimizer = optim.SGD(model.parameters(), lr=args.learning_rate)
 
-# train
-for pass_num in range(23): # TODO: training longer helps
+    def eval_split(split):
+        # eval the full train/test set, batched implementation for efficiency
+        model.eval()
+        X, Y = (Xtr, Ytr) if split == 'train' else (Xte, Yte)
+        Yhat = model(X)
+        loss = torch.mean((Y - Yhat)**2)
+        err = torch.mean((Y.argmax(dim=1) != Yhat.argmax(dim=1)).float())
+        print(f"eval: split {split:5s}. loss {loss.item():e}. error {err.item()*100:.2f}%. misses: {int(err.item()*Y.size(0))}")
+        writer.add_scalar(f'error/{split}', err.item()*100, pass_num)
+        writer.add_scalar(f'loss/{split}', loss.item(), pass_num)
 
-    # perform one epoch of training
-    model.train()
-    for step_num in range(Xtr.size(0)):
+    # train
+    for pass_num in range(23):
 
-        # fetch a single example into a batch of 1
-        x, y = Xtr[[step_num]], Ytr[[step_num]]
+        # perform one epoch of training
+        model.train()
+        for step_num in range(Xtr.size(0)):
 
-        # forward the model and the loss
-        yhat = model(x)
-        loss = torch.mean((y - yhat)**2)
+            # fetch a single example into a batch of 1
+            x, y = Xtr[[step_num]], Ytr[[step_num]]
 
-        # calculate the gradient and update the parameters
-        optimizer.zero_grad(set_to_none=True)
-        loss.backward()
-        optimizer.step()
+            # forward the model and the loss
+            yhat = model(x)
+            loss = torch.mean((y - yhat)**2)
 
-    # after epoch epoch evaluate the train and test error / metrics
-    print(pass_num + 1)
-    eval_split('train')
-    eval_split('test')
+            # calculate the gradient and update the parameters
+            optimizer.zero_grad(set_to_none=True)
+            loss.backward()
+            optimizer.step()
+
+        # after epoch epoch evaluate the train and test error / metrics
+        print(pass_num + 1)
+        eval_split('train')
+        eval_split('test')
+
+    # save final model to file
+    torch.save(model.state_dict(), os.path.join(args.output_dir, 'model.pt'))
